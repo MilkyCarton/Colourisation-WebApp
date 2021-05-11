a6b5696eaed1d32323a747881a5eb1dc7174184d
Oliver <milkycarton@users.noreply.github.com>
2021-05-11 11:00:57 +0100

    Updated colourmodel.py

--- colourmodel.py
+++ colourmodel.py
@@ -21,23 +21,22 @@ def conversion(filename):
     colourise = []
     print('Output of the Model')
     image = img_to_array(load_img(os.path.join(__DIR__, "static/img/uploads/" + filename)))
-    colorize.append(resize(image, (256, 256), anti_aliasing=True))
+    colourise.append(resize(image, (256, 256), anti_aliasing=True))
 
-    colorize = np.array(colorize, dtype=float)
-    colorize = rgb2lab(1.0 / 255 * colorize)[:, :, :, 0]
-    colorize = colorize.reshape(colorize.shape + (1,))
+    colourise = np.array(colourise, dtype=float)
+    colourise = rgb2lab(1.0 / 255 * colourise)[:, :, :, 0]
+    colourise = colourise.reshape(colourise.shape + (1,))
 
     # Test model
-    output = loaded_model.predict(colorize)
+    output = loaded_model.predict(colourise)
     output = output * 128
 
     # Output colourisations
     for i in range(len(output)):
         cur = np.zeros((256, 256, 3))
-        cur[:, :, 0] = colorize[i][:, :, 0]
+        cur[:, :, 0] = colourise[i][:, :, 0]
         cur[:, :, 1:] = output[i]
         resImage = lab2rgb(cur)
         imsave(os.path.join(__DIR__, "static/img/converted/"+filename), resImage)
         os.remove(os.path.join(__DIR__, "static/img/uploads/"+filename))
-
         return "static/img/converted/"+filename
